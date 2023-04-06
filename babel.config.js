fc1fff04bf6ffb48183143a3b908252f82a31898
Gabriel De Marco <lino.dmarco@gmail.com>
2023-04-06 18:38:11 -0300

    feat: add navigation

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
+};
