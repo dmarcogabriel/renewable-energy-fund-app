f80f33745a6bc58959ea3df1cc227df6b8af2243
Gabriel De Marco <lino.dmarco@gmail.com>
2023-04-10 14:48:31 -0300

    feat(funds): add fund mock data

--- src/models/IFundInterface.ts
+++ src/models/IFundInterface.ts
@@ -1,1 +1,28 @@
-export interface IFund {}
+type IFundData = {[key: string]: number};
+
+interface IFundInfoAndStats {
+  aum: number;
+  vintageRange: {
+    min: number;
+    max: number;
+  };
+  priceAtClose: number;
+  issueDate: string;
+  ter: number;
+  priceAtOpen: number;
+}
+
+export interface IFund {
+  totalAmount: number;
+  year: number;
+  earningsPercentage: number;
+  data: {
+    hour: IFundData[];
+    day: IFundData[];
+    week: IFundData[];
+    month: IFundData[];
+    year: IFundData[];
+    all: IFundData[];
+  };
+  infoAndStats: IFundInfoAndStats;
+}
